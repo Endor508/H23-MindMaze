76ea137368cba96b60e4fe39f87cae27efd9db47
Renoud <2147416@bdeb.qc.ca>
2023-05-03 14:17:47 -0400

    script enigmes

---
+++ Assets/Scripts/InteractEnigme.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractEnigme : MonoBehaviour
+{
+    private Enigme enigme;
+    [SerializeField] private Transform playerCameraTransform;
+    [SerializeField] private Transform objectGrabPointTransform;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (enigme == null)
+            {
+                float interactDistance = 5f;
+                if (Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out RaycastHit raycastHit, interactDistance))
+                {
+                    if (raycastHit.transform.TryGetComponent(out enigme))
+                    {
+                        enigme.afficherEnigme();
+                    }
+                }
+            }
+            else
+            {
+                enigme.cacherEnigme();
+                enigme = null;
+            }
+        }
+    }
+}
