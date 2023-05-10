a598b09eae2555781ca3a4b6e856400ecf772b40
Renoud <2147416@bdeb.qc.ca>
2023-05-10 13:23:21 -0400

    Enigme Fin
    systeme d'enigme fonctionne, il ne manque que le score, le timer, un improvement de l'ui (rouge si mauvais, vert si bon), puis afficher la clé a partir de 5 point donc 5 enigmes reussies.

--- Assets/Scripts/Enigme.cs
+++ Assets/Scripts/Enigme.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.IsolatedStorage;
 using System.Linq;
 using TMPro;
 using UnityEngine;
@@ -16,11 +17,23 @@ public class Enigme : MonoBehaviour
     [SerializeField] private TextMeshProUGUI b;
     [SerializeField] private TextMeshProUGUI c;
     [SerializeField] private TextMeshProUGUI d;
-    private string boutonNom;
+
+    
+    [SerializeField] private Button A;
+    [SerializeField] private Button B;
+    [SerializeField] private Button C;
+    [SerializeField] private Button D;
+
+    private Button boutonSolution;
+    private List<Button> listButton = new List<Button>();
+
+
+
+
     private string solution;
 
 
-    void Start()
+    void Awake()
     {
         string path = "Assets/Scripts/enigmes.txt";
         string line = File.ReadLines(path).Skip(id - 1).Take(1).First();
@@ -28,8 +41,24 @@ public class Enigme : MonoBehaviour
 
         question.text = list[0]; a.text = list[1]; b.text = list[2]; c.text = list[3]; d.text = list[4]; solution = list[5];
 
-        boutonNom = "Option " + solution;
+        listButton.Add(A); listButton.Add(B); listButton.Add(C); listButton.Add(D);
+
         
+        for(int i = 0; i < listButton.Count; i++)
+        {
+            if (listButton[i].name == solution)
+            {
+                listButton[i].onClick.AddListener(delegate { TaskOnClickReussi(); });
+            }
+            else
+            {
+                listButton[i].onClick.AddListener(delegate { TaskOnClickEchec(); });
+            }
+        }
+            
+        
+
+
     }
 
     public void afficherEnigme()
@@ -50,15 +79,21 @@ public class Enigme : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    void TaskOnClick()
+    void TaskOnClickReussi()
     {
         Debug.Log("oui");
     }
 
+    void TaskOnClickEchec()
+    {
+        Debug.Log("non");
+    }
+
     // Update is called once per frame
     void Update()
-    {
        
+    {
+        
     }
 
 
