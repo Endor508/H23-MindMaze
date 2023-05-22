c56b0a35330b4f3f40af7e486033fe0d0e8eed03
ammarranko <ammar.ranko707@gmail.com>
2023-05-22 12:40:42 -0400

    Menu Jeu + changement de scène

--- Assets/Scripts/MenuPrincipal.cs
+++ Assets/Scripts/MenuPrincipal.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MenuPrincipal : MonoBehaviour
 {
-    public string nomSceneJeu = "Player Test";
-    
-    public void PlayGame(){
+    public string nomSceneJeu = "MathV.2";
+    public string nomSceneJeu1 = "StartingScene";
+
+   
+    public void PlayHistoire(){
+        SceneManager.LoadScene(nomSceneJeu1);
+    }
+
+   public void PlayMath(){
         SceneManager.LoadScene(nomSceneJeu);
     }
     public void QuitGame(){
